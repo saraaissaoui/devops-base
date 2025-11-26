ac50b7ddf264bd6e8174dfbc4a8308e395ac2220
Aissaoui <sara.aissaoui@edu.esiee.fr>
2025-11-26 19:04:26 +0100

    Update response text in test

--- td5/scripts/sample-app/app.js
+++ td5/scripts/sample-app/app.js
@@ -1,14 +1,29 @@
 const express = require('express');
-
 const app = express();
+
 app.set('view engine', 'ejs');
 
+// Route racine
 app.get('/', (req, res) => {
   res.send('DevOps Labs!');
 });
 
+// Petite fonction pour échapper les caractères dangereux
+function sanitize(str) {
+  return str
+    .replace(/&/g, '&amp;')
+    .replace(/</g, '&lt;')
+    .replace(/>/g, '&gt;')
+    .replace(/"/g, '&quot;')
+    .replace(/'/g, '&#39;');
+}
+
+// Route /name/:name
 app.get('/name/:name', (req, res) => {
-  res.render('hello', {name: req.params.name});
+  const rawName = req.params.name;          // ce que l’utilisateur envoie
+  const safeName = sanitize(rawName);       // on nettoie
+  res.send(`Hello, ${safeName}!`);          // on renvoie la version safe
 });
 
 module.exports = app;
+
